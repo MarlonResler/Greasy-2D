71605eb0ff7dfe841148e7acdbb171214062318b
Simon Anderka <johndoe@example.com>
2020-12-02 14:06:11 +0100

    Änderung gespeichert

---
+++ Greasy Unity/Assets/neustart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement
+
+public class neustart : MonoBehaviour
+{
+  void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "maincharacter")
+        {
+            Debug.Log("maincharacter dead");
+            SceneManager.LoadScene("scene00");
+        }else
+            Debug.Log("Its not the maincharacter")
+    }
+}
