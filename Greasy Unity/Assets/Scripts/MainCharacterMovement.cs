027bbbe22afef386b4c74a7118a7706d647ea4c5
Dani <danikk01@gmx.de>
2020-12-02 08:54:02 +0100

    add lvl2 changes (patrol enemy/ coin UI/ coins/ BGM test)

---
+++ Greasy Unity/Assets/Scripts/MainCharacterMovement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    Transform transObj;
+
+    [SerializeField]
+    Rigidbody rig;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transObj.Translate(Vector3.right * Input.GetAxis("Horizontal") * 0.20f);
+        if (Input.GetAxis("Vertical") > 0)
+        
+        {
+            transObj.Translate(Vector3.up * Input.GetAxis("Vertical") * 0.30f);
+     
+        }
+    }
+}
