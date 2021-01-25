70e441e1c5b78f72e10e2e73c38a374121ea32d7
mr-development <marlon.resler@gmail.com>
2021-01-25 20:12:19 +0100

    Add file open methods and JSON parser

--- Greasy Unity/Assets/Scripts/GameSaver.cs
+++ Greasy Unity/Assets/Scripts/GameSaver.cs
@@ -1,36 +1,48 @@
 
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
 public static class SaveFileSerializer{
-    private string filePath = "saveFile.json";
-    public SaveFile OpenSaveFile(){
+    private static string FILE_PATH = "saveFile.json";
+    public static SaveFile OpenSaveFile(){
         try
         {
-            StreamWriter sw = new StreamWriter(File.Open(this.filePath, System.IO.FileMode.Append))
+            StreamReader sr = new StreamReader(File.Open(SaveFileSerializer.FILE_PATH, System.IO.FileMode.Append));
+            SaveFile save = JsonUtility.FromJson<SaveFile>(sr.ReadToEnd());
+            return save;
         }
         catch (System.Exception)
         {
             
             throw;
+            return new SaveFile();
         }
     }
 
-    public SaveFile Deserialize(){
+    public static SaveFile Deserialize(){
+        
+    }
+
+    public static string Serialize(SaveFile save){
 
     }
 
-    public string Serialize(SaveFile save){
-
-    }
-
-    public void WriteSaveFile(string jsonString){
+    public static void WriteSaveFile(string jsonString){
+        StreamWriter sw;
         try
         {
-            StreamWriter sw = new StreamWriter(File.Open(this.filePath, System.IO.FileMode.Append))
+            sw = new StreamWriter(File.Open(this.filePath, System.IO.FileMode.Append));
         }
         catch (System.Exception)
         {
             
             throw;
+            return;
         }
+
+        sw.Write(JsonUtility.ToJson(jsonString));
     }
 }
 
@@ -38,20 +50,22 @@ public class SaveFile {
     private Dictionary<string, bool> skins;
     private int coins;
 
-    SaveFile(){
-        this skins = new Dictionary<string, bool>();
+    public SaveFile() {
+        this.skins = new Dictionary<string, bool>();
         skins.Add("Black", false);
         skins.Add("Bacon", false);
         skins.Add("Merkel", false);
         skins.Add("Dog", false);
         skins.Add("Mario", false);
+
+        this.coins = 0;
     }
 
-    ActivateSkin(string skin){
+    void ActivateSkin(string skin){
         this.skins[skin] = true;
     }
 
-    SetCoins(int coinsReached){
+    void SetCoins(int coinsReached){
         this.coins += coinsReached;
     }
 }
