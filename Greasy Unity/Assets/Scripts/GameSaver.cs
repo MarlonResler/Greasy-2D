78b20c79c1aefe61f15b3a4a8e0073f323ac94fb
mr-development <marlon.resler@gmail.com>
2021-01-28 01:45:20 +0100

    Add GameSaver to every Level and direct coins in save file

--- Greasy Unity/Assets/Scripts/GameSaver.cs
+++ Greasy Unity/Assets/Scripts/GameSaver.cs
@@ -4,41 +4,52 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+public class GameSaver : MonoBehaviour {
+    SaveFile save;
+    void Start(){
+        save = SaveFileSerializer.OpenSaveFile();
+
+    }
+
+    public void addCoins(int coins){
+        if (save != null)
+        {
+            save.SetCoins(coins);
+            Debug.Log("Saved Coins. New value: " + save.GetCoins());
+        } else {
+            Debug.LogError("Save file not assigned");
+        }
+        
+    }
+}
+
 public static class SaveFileSerializer{
     private static string FILE_PATH = "saveFile.json";
     public static SaveFile OpenSaveFile(){
         try
         {
-            StreamReader sr = new StreamReader(File.Open(SaveFileSerializer.FILE_PATH, System.IO.FileMode.Append));
+            StreamReader sr = new StreamReader(File.Open(SaveFileSerializer.FILE_PATH, System.IO.FileMode.OpenOrCreate, FileAccess.Read));
             SaveFile save = JsonUtility.FromJson<SaveFile>(sr.ReadToEnd());
+            Debug.Log("Successfully opened Save File");
             return save;
         }
-        catch (System.Exception)
-        {
-            
-            throw;
+        catch (System.Exception e)
+        {            
+            Debug.LogError(e);
             return new SaveFile();
         }
-    }
-
-    public static SaveFile Deserialize(){
-        
-    }
-
-    public static string Serialize(SaveFile save){
-
     }
 
     public static void WriteSaveFile(string jsonString){
         StreamWriter sw;
         try
         {
-            sw = new StreamWriter(File.Open(this.filePath, System.IO.FileMode.Append));
+            sw = new StreamWriter(File.Open(FILE_PATH, System.IO.FileMode.Append));
         }
-        catch (System.Exception)
+        catch (System.Exception e)
         {
             
-            throw;
+            Debug.LogError(e);
             return;
         }
 
@@ -61,11 +72,15 @@ public class SaveFile {
         this.coins = 0;
     }
 
-    void ActivateSkin(string skin){
+    public void ActivateSkin(string skin){
         this.skins[skin] = true;
     }
 
-    void SetCoins(int coinsReached){
+    public void SetCoins(int coinsReached){
         this.coins += coinsReached;
+    }
+
+    public int GetCoins(){
+        return this.coins;
     }
 }
